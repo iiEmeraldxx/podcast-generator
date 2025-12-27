8844a7a6b7870eed6fa79eb31374c5ad6e5e6e55
iiEmeraldxx <76895762+iiEmeraldxx@users.noreply.github.com>
2025-12-27 11:22:12 +0000

    edited feed.py to stablize virtul environment code

--- feed.py
+++ feed.py
@@ -1,7 +1,7 @@
 import yaml
 import xml.etree.ElementTree as xml_tree
 
-with open('feed.yaml', 'r') as file:
+with open('/github/workspace/feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
     rss_element = xml_tree.Element('rss', {'version':'2.0', 
